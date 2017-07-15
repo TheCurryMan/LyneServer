a9a2fce847330a2641144b7e67d05605da095778
TheCurryMan <avinashkalpitjain@gmail.com>
2017-07-15 00:25:21 -0700

    Server Code for Lyne App

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+import requests
+import json
+from firebase import firebase
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+
+    userID = request.args.get('userID')
+    lyneID = request.args.get('lyneID')
+    fb = firebase.FirebaseApplication('https://lyne.firebaseio.com/', None)
+    data = fb.get('/lynes/'+lyneID, None)
+
+    title = data["name"]
+        header = {"Content-Type": "application/json; charset=utf-8",
+              "Authorization": "Basic NmIxNmQwNjQtNjUxMi00MTg4LWFlOGMtZjE5YTFlMDdkYWY1"}
+
+    payload = {"app_id": "87a88c34-ce46-41bb-bcbe-867248438aaa",
+               "include_player_ids": [userID],
+               "contents": {"en": "It's now your turn. You are 1st in Lyne!"},
+               "headings": {"en": title},
+               "ios_badgeType": "Increase",
+               "ios_badgeCount": "1",
+               }
+
+    req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
+
+    print(req.status_code, req.reason)
+
+    return title + " " + userID
+
+
+if __name__ == '__main__':
+    app.run()
